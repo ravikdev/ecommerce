c033b09df2407a610343c35777764d28108ef067
ravikdev <ravikumar71996@gmail.com>
2023-12-02 11:17:08 +0530

    Create folder strucutre,model and hashpassword

--- helper/authhelper.js
+++ helper/authhelper.js
@@ -1,14 +1,15 @@
 import bcrypt from bcrypt; 
 export const hashPassword = async(password) =>{
-try{
-    const salt =10;
-    const hashedPassword = bcrypt.hash(password,salt);
-}
-catch(error){
-console.log(error);
-}
+    try{
+        const salt =10;
+        const hashedPassword = bcrypt.hash(password,salt);
+        return hashedPassword;
+    }
+    catch(error){
+    console.log(error);
+    }
 };
 
-export const comparePassword = async(password,hashPassword)=>{
-    return bcrypt.compare(password,hashedPassword);
+export const comparePassword = async(password,hashedPassword)=>{
+    return bcrypt.compare(password, hashedPassword);
 };
